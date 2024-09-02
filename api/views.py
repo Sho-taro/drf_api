909a8b741f86e1552c85e59cdac85f6489ebe518
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-09-02 10:27:25 +0900

    [add]
    viewを追加（作業未完了）

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import generics, viewsets
+from django.contrib.auth.models import User
+from .models import Task
+from .serializers import UserSerializer, TaskSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    pass
